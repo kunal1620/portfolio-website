0d01f5e47658ef54974386cdf5c45b36ee293716
Kunal Pawar <kunal1620@gmail.com>
2020-10-15 01:19:34 +1100

    Updates

---
+++ precache-manifest.e7795c96abd0c856bd7256adba332dae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19e28850df01945c14cba3e96c94bbf",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "1e42e8d568288fc1328f",
+    "url": "/portfolio-website/static/css/main.121f1119.chunk.css"
+  },
+  {
+    "revision": "64539d8c80ce78904c7b",
+    "url": "/portfolio-website/static/js/2.ced538ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-website/static/js/2.ced538ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e42e8d568288fc1328f",
+    "url": "/portfolio-website/static/js/main.e63ed394.chunk.js"
+  },
+  {
+    "revision": "fb02826a0696d07c6467",
+    "url": "/portfolio-website/static/js/runtime-main.1df25937.js"
+  },
+  {
+    "revision": "48b41062a5bb533b90f3e9a72cf0f3b2",
+    "url": "/portfolio-website/static/media/HindVadodara-Bold.48b41062.ttf"
+  },
+  {
+    "revision": "b4f1bcc3a00b893fc6e28b8882d24b0b",
+    "url": "/portfolio-website/static/media/HindVadodara-Light.b4f1bcc3.ttf"
+  },
+  {
+    "revision": "571c4180d3806576198c455aae5cfe27",
+    "url": "/portfolio-website/static/media/HindVadodara-Medium.571c4180.ttf"
+  },
+  {
+    "revision": "77886706d5e86b55342fb1c0815f48ee",
+    "url": "/portfolio-website/static/media/HindVadodara-Regular.77886706.ttf"
+  },
+  {
+    "revision": "5a6a6f006309eb029068b20f3b3b22e7",
+    "url": "/portfolio-website/static/media/HindVadodara-SemiBold.5a6a6f00.ttf"
+  }
+]);
